2036b3f4f6c746f718521f779f63bbee0bea9bdf
krisitown <krisipwn@gmail.com>
2016-07-27 17:28:34 +0300

    finishing touches

--- app/src/main/java/com/couphelper/krisitown/couphelper/MainActivity.java
+++ app/src/main/java/com/couphelper/krisitown/couphelper/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -40,13 +41,43 @@ public class MainActivity extends AppCompatActivity {
         addPlayerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText editText = new EditText(context);
+                final EditText editText = new EditText(context);
                 editText.setLayoutParams(new LinearLayout.LayoutParams(
-                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT,
                         LinearLayout.LayoutParams.WRAP_CONTENT
                 ));
-                int currentId = editText.generateViewId();
-                layout.addView(editText);
+                final int currentId = editText.generateViewId();
+                final LinearLayout nameAndCross = new LinearLayout(context);
+                nameAndCross.setLayoutParams(new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT,
+                        LinearLayout.HORIZONTAL));
+                nameAndCross.setGravity(Gravity.CENTER);
+
+
+                Button button = new Button(context);
+                button.generateViewId();
+                button.setText("X");
+                button.setLayoutParams(new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.WRAP_CONTENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT
+                ));
+
+                editText.setWidth(360);
+
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        layout.removeView(nameAndCross);
+                        playerNames.remove(editText);
+                    }
+                });
+
+
+                nameAndCross.addView(editText);
+                nameAndCross.addView(button);
+
+                layout.addView(nameAndCross);
                 playerNames.add(editText);
             }
         });
